3f470ec5b491109fd5f211639b4d1ebfdcc6c0a7
Erumusuf <160308293+Erumusuf@users.noreply.github.com>
2024-12-09 11:29:13 -0800

    Add files via upload

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import "../css/contact.css"
+
+function Blog() {
+  return (
+<div className='cont-body'>
+    <div className='cont-div1'>
+        <img className='cont-image' src="/Meubel_Logos-05.png"></img>
+        <h2 className='cont-heading'>Blog</h2>
+        <div className='cont-subdiv'>
+            <h2>Home</h2>
+            <img src="/dashicon.png"></img>
+            <h2>Blog</h2>
+        </div>
+    </div>
+
+    <div className='flex p-[5rem] h-auto'>
+        <img src="/Group176.png"></img>
+        <div>
+          <img src='Group171.png'></img>
+          <img src='Group184.png'></img>
+        </div>
+    </div>
+    <div className='flex justify-center' ><img src="/Group72.png"></img></div>
+    <div className='flex justify-center' ><img src="/Frame161.png"></img></div>
+    
+    
+
+</div>
+  )
+}
+
+export default Blog
